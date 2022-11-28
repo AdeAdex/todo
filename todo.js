90c51f0ce1acb67e5905744de97ac3dcead6b7c8
Amole Adeolu <98040608+Adex2210@users.noreply.github.com>
2022-11-28 08:21:23 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,17 +1,53 @@
 let myFruits = []
 function addTodo() {
-    myFruits.push(myInput.value);
+    if (myInput.value == "") {
+        alertMessage.innerHTML = `Please enter a Todo to be added`;
+        alertMessage.style.backgroundColor = "rgb(247, 204, 204)"
+        alertMessage.style.color = "rgb(255, 0 ,0)"
+        alertMessage.style.visibility = "visible"
+    } else {
+        myFruits.push(myInput.value);
+        alertMessage.innerHTML = `New Todo added successfully`
+        alertMessage.style.backgroundColor = "rgba(200, 247, 197, .5)"
+        alertMessage.style.color = "green"
+        alertMessage.style.visibility = "visible"
+    }
     newTask()
 }
 
 function delTodo(userIndex) {
-    myFruits.splice(userIndex, 1);
+    var userResponse = confirm(`Are you sure you want to delete this Todo? This action cannot be undone`);
+    if (userResponse == true) {
+        myFruits.splice(userIndex, 1);
+        alertMessage.innerHTML = `Todo Deleted Successfully!`
+        alertMessage.style.backgroundColor = "rgb(247, 204, 204)"
+        alertMessage.style.color = "rgb(255, 0 ,0)"
+        alertMessage.style.visibility = "visible"
+       
+    } else {
+        myOutput.innerHTML =  myFruits;
+        alertMessage.innerHTML = `You just cancelled, your Todo is not deleted`
+        alertMessage.style.backgroundColor = "rgba(200, 247, 197, .5)"
+        alertMessage.style.color = "green"
+        alertMessage.style.visibility = "visible"
+    }
     newTask()
 };
 
 
 function editTodo(userIndex) {
-    myFruits.splice(userIndex, 1, myInput.value)
+    if (myInput.value == "") {
+        alertMessage.innerHTML = `Please enter a Todo to be updated`;
+        alertMessage.style.backgroundColor = "rgb(247, 204, 204)"
+        alertMessage.style.color = "rgb(255, 0 ,0)"
+        alertMessage.style.visibility = "visible"
+    } else {
+        myFruits.splice(userIndex, 1, myInput.value)
+        alertMessage.innerHTML = `Your Todo has been updated successfully`
+        alertMessage.style.backgroundColor = "rgba(200, 247, 197, .5)"
+        alertMessage.style.color = "green"
+        alertMessage.style.visibility = "visible"
+    }
     newTask();
 }
 
