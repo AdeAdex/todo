f457d4e27eb4c13deedd775e275ee5cd224a73ca
Adeolu Amole <adeoluamole@gmail.com>
2023-10-26 11:44:25 +0100

    Making correction to application

--- todo.js
+++ todo.js
@@ -103,9 +103,6 @@
 
 
 
-
-
-
 let myFruits = JSON.parse(localStorage.getItem('myFruits')) || [];
 
 function addTodo() {
@@ -114,12 +111,12 @@ function addTodo() {
   if (myInput.value === '') {
     alertMessage.innerHTML = `Please enter a Todo to be added`;
     alertMessage.style.backgroundColor = 'rgb(247, 204, 204)';
-    alertMessage.style.color = 'rgb(255, 0 ,0)';
+    alertMessage.style.color = 'rgb(255, 0, 0)';
     alertMessage.style.visibility = 'visible';
   } else {
     myFruits.push(myInput.value);
     alertMessage.innerHTML = `New Todo added successfully`;
-    alertMessage.style.backgroundColor = 'rgba(200, 247, 197, .5)';
+    alertMessage.style.backgroundColor = 'rgba(200, 247, 197, 0.5)';
     alertMessage.style.color = 'green';
     alertMessage.style.visibility = 'visible';
 
@@ -136,14 +133,14 @@ function delTodo(userIndex) {
     myFruits.splice(userIndex, 1);
     alertMessage.innerHTML = `Todo Deleted Successfully!`;
     alertMessage.style.backgroundColor = 'rgb(247, 204, 204)';
-    alertMessage.style.color = 'rgb(255, 0 ,0)';
+    alertMessage.style.color = 'rgb(255, 0, 0)';
     alertMessage.style.visibility = 'visible';
 
     // Save the updated myFruits array to localStorage
     localStorage.setItem('myFruits', JSON.stringify(myFruits));
   } else {
-    alertMessage.innerHTML = `You just cancelled, your Todo is not deleted`;
-    alertMessage.style.backgroundColor = 'rgba(200, 247, 197, .5)';
+    alertMessage.innerHTML = `You just canceled, your Todo is not deleted`;
+    alertMessage.style.backgroundColor = 'rgba(200, 247, 197, 0.5)';
     alertMessage.style.color = 'green';
     alertMessage.style.visibility = 'visible';
   }
@@ -153,15 +150,15 @@ function delTodo(userIndex) {
 function editTodo(userIndex) {
   const myInput = document.getElementById('myInput');
 
-  if (myInput.value == '') {
+  if (myInput.value === '') {
     alertMessage.innerHTML = `Please enter a Todo to be updated`;
     alertMessage.style.backgroundColor = 'rgb(247, 204, 204)';
-    alertMessage.style.color = 'rgb(255, 0 ,0)';
+    alertMessage.style.color = 'rgb(255, 0, 0)';
     alertMessage.style.visibility = 'visible';
   } else {
     myFruits.splice(userIndex, 1, myInput.value);
     alertMessage.innerHTML = `Your Todo has been updated successfully`;
-    alertMessage.style.backgroundColor = 'rgba(200, 247, 197, .5)';
+    alertMessage.style.backgroundColor = 'rgba(200, 247, 197, 0.5)';
     alertMessage.style.color = 'green';
     alertMessage.style.visibility = 'visible';
 
@@ -183,9 +180,9 @@ function newTask() {
   myInput.value = '';
   for (let index = 0; index < myFruits.length; index++) {
     myOutput.innerHTML += `
-         <div class = "col">
-            <h1>${myFruits[index]} </h1>
-            <div class= "actionBtn">
+         <div class="col">
+            <h1>${myFruits[index]}</h1>
+            <div class="actionBtn">
                 <button class="warning" onclick="editTodo(${index})">Edit Todo</button>
                 <button class="danger" onclick="delTodo(${index})">Delete Todo</button>
             </div>
@@ -193,10 +190,11 @@ function newTask() {
   }
 }
 
-
 function fetchFromLocalStorage() {
+  if (!myFruits) {
+    myFruits = [];
+  }
   if (myFruits.length > 0) {
     newTask();
   }
 }
-
